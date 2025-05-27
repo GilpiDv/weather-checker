39ca77803427ecf17ea6a68ead90872a9e264ad7
GilpiDv <gilgonzalez981@gmail.com>
2025-05-27 14:44:01 -0600

    Add validations to our form

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,10 +1,36 @@
+import { useState, type ChangeEvent, type FormEvent } from "react";
 import { countries } from "../../data/countries";
 import styles from "./Form.module.css"
+import type { SearchType } from "../../types";
+import Alert from "../Alert/Alert";
 
 export default function Form() {
 
+    const [search, setSearch] = useState<SearchType>({
+        city: '',
+        country: ''
+    });
+
+    const [alert, setAlert] = useState('');
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>) => {
+        setSearch({
+            ...search,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        if(Object.values(search).includes('')) {
+            setAlert('All fields are required')
+        }
+    }
+
     return (
-        <form className={styles.form}>
+        <form className={styles.form} onSubmit={handleSubmit}>
+            {alert && <Alert>{alert}</Alert>}
             <div className={styles.field}>
                 <label htmlFor="city">City:</label>
                 <input 
@@ -12,11 +38,18 @@ export default function Form() {
                     id="city"
                     name="city"
                     placeholder="City" 
+                    value={search.city}
+                    onChange={handleChange}
                 />
             </div>
             <div className={styles.field}>
-                <label htmlFor="countries">Countries:</label>
-                <select name="countries" id="countries">
+                <label htmlFor="country">Country:</label>
+                <select 
+                    id="country" 
+                    name="country" 
+                    value={search.country} 
+                    onChange={handleChange}
+                >
                     <option value="">Select country</option>
                     {countries.map(country => (
                         <option 
